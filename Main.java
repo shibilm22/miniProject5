1943c4f37a5a2451e843b30f0af84a4ad3592ea9
shibilm22 <shibil.mandawe@patelhs.org>
2021-02-22 14:13:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome, what's the capacity for your event ");
+    int capacity = scan.nextInt(); 
+    concert(capacity);
+  
+  }
+
+  static void concert(int capicity)
+  {
+   if (capacity == 5);
+  }
+}
