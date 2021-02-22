a09a5d0be73c8517d4d88b04ef589f633639e9d8
shibilm22 <shibil.mandawe@patelhs.org>
2021-02-22 23:51:58 +0000

    added if else statement

--- Main.java
+++ Main.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome, what's the capacity for your event ");
-    int capacity = scan.nextInt(); 
-    concert(capacity);
+    System.out.println("Welcome to Shibil's concert. What is your name? " );
+    String name = scan.next(); 
+    int capacity = 5;
+    concert(name, capacity);
   
   }
 
-  static void concert(int capicity)
+  static boolean concert(String name, int capacity)
   {
-   if (capacity == 5);
+   if (capacity == 5){
+     System.out.println("You are allowed in the concert!");
+     return true;
+   }else{
+     System.out.println("The concert is at full capacity! Maybe next time.");
+     return false; 
+   }
   }
 }
