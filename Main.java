1f75fa26a2af2492632308bae80b0aa568730c20
shibilm22 <shibil.mandawe@patelhs.org>
2021-02-23 18:49:21 +0000

    Ready to Submit

--- Main.java
+++ Main.java
@@ -5,22 +5,34 @@ class Main {
     Scanner scan = new Scanner(System.in);
     //recieving user input from the user
     System.out.println("Welcome to Shibil's concert. What is your name? " );
+    //Welcoming people to the concert using a print statement
     String name = scan.next(); 
-    int capacity = 15; 
+    //saving and reciving name into type string 
+    int numPpl = 0;
+    int capacity = 15;
+    //capacity will be full at 15 people 
     concert(capacity, numPpl);
+    //calling concert method back into the main method including parametrs 
 
   }
-
-  static boolean concert(int numPpl, int capacity)
+  //method called concert
+  static boolean concert(int capacity, int numPpl)
   {
+    Scanner scan = new Scanner(System.in);
    if (capacity > 0 ){
+    capacity = capacity - numPpl;
+     //base case where capacity mus be greater than 0 
      System.out.println("More room in concert. How many of you are joining?");
-     capacity = capacity - numPpl;
-     concert(capacity, numPpl); 
-     return true;
+      numPpl = scan.nextInt(); 
+     //if it is greater than zero then it will print this statement
+     //for every person joining capacity will subject one
+     return concert(capacity, numPpl); 
+     //only if capacity still has space 
    }else{
      System.out.println("Sorry,the concert is at full capacity! Maybe next time.");
+     //will print once capacity hits to 15
      return false; 
+     //once capacity hits to 15 it will no longer have space
    }
   }
 }
