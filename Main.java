55bf079807efa06840e72c3169eb697d5c157711
shibilm22 <shibil.mandawe@patelhs.org>
2021-02-23 18:19:46 +0000

    added comment

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    //recieving user input from the user
     System.out.println("Welcome to Shibil's concert. What is your name? " );
     String name = scan.next(); 
-    int capacity = 15;
+    int capacity = 15; 
     concert(capacity, numPpl);
-  
+
   }
 
   static boolean concert(int numPpl, int capacity)
@@ -15,6 +16,7 @@ class Main {
    if (capacity > 0 ){
      System.out.println("More room in concert. How many of you are joining?");
      capacity = capacity - numPpl;
+     concert(capacity, numPpl); 
      return true;
    }else{
      System.out.println("Sorry,the concert is at full capacity! Maybe next time.");
